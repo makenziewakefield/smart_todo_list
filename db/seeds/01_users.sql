998dae3d1e660229a3b3ef2f584313cdadf2ac5d
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-06-20 15:25:59 -0400

    Add database and set up routes for user and items

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,17 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+
+INSERT INTO users (name, email, password)
+VALUES ('Nihil eius', 'e.nihil@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('John Doe', 'john.doe@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('Jane Smith', 'jane.smith@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('Alice Johnson', 'alice.johnson@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO users (name, email, password)
+VALUES ('Bob Brown', 'bob.brown@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories (name)
+VALUES ('Film / Series');
+INSERT INTO categories (name)
+VALUES ('Eateries');
+INSERT INTO categories (name)
+VALUES ('Books');
+INSERT INTO categories (name)
+VALUES ('Products');
+
