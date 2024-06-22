57be65898bdafdc12e16b547dd947ea5627fa4ba
Krisan Manoharan <krisan1010@hotmail.com>
2024-06-22 10:48:58 -0400

    Add express, router and connect aiHelpers to categorize.js

---
+++ routes/categorize/categorize.js
@@ -0,0 +1,14 @@
+// Web Server Config
+const express = require('express');
+const router  = express.Router();
+
+//Helpers
+const {categorizeItem, itemIntoDatabase} = require('../../helpers/aiHelper')
+
+
+
+router.post('/', (req, res) => {
+  
+});
+
+module.exports = router;
